bbfbc91cbedc0f245d22eaaf14cc084f2aedc219
mikevgb <75221782+mikevgb@users.noreply.github.com>
2021-07-05 16:32:05 +0200

    ex 08, rock, paper, scissors

---
+++ 08_rock_paper_scissors.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using 
+# input), compare them, print out a message of congratulations to the winner, 
+# and ask if the players want to start a new game)
+
+# Remember the rules:
+
+# Rock beats scissors
+# Scissors beats paper
+# Paper beats rock
+
+import os
+
+player1 = raw_input("Player 1, please enter your move: ")
+player2 = raw_input("Player 2, please enter your move: ")
+move_list = ["rock", "scissors", "paper"]
+
+if player1 in move_list and player2 in move_list:
+	if player1 == str("rock") and player2 == str("scissors"):
+		print("Player 1 wins ")
+	if player1 == str("scissors") and player2 == str("rock"):
+		print("Player 2 wins ")
+	if player1 == str("scissors") and player2 == str("paper"):
+		print("Player 1 wins ")
+	if player1 == str("paper") and player2 == str("scissors"):
+		print("Player 2 wins ")
+	if player1 == str("paper") and player2 == str("rock"):
+		print("Player 1 wins ")
+	if player1 == str("rock") and player2 == str("paper"):
+		print("Player 2 wins ")
+else:
+	print ("Bad input, please enter rock, scissors or paper")
+
+restart = raw_input("Want to play again?(y/n) ")
+if restart == str("y"):
+	os.system("python 08_rock_paper_scissors.py 1")
+
